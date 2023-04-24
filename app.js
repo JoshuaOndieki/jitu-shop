4a70c74d6d866d39f2bc71453ac169893b4cdc39
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-24 16:18:33 +0300

    test log os.hostname

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const os = require('os')
 const app = express()
 const port = 8080
 
@@ -10,5 +11,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
+  console.log(os.hostname());
     console.log('Listening on port: ' + port)
 });
