b04c9e0adff7535552e8e65a9a0879e2ab2211b0
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-24 21:10:17 +0300

    Attempt#6 try listening json-server on localhost:3000

--- app.js
+++ app.js
@@ -2,7 +2,41 @@ const express = require('express')
 const path = require('path')
 const os = require('os')
 const app = express()
+require('dotenv').config()
 const APPPORT = process.env.APPPORT || 8080
+
+
+function ipHostCheck() {
+  const https = require('https');
+
+  https.get(`https://api.ipdata.co?api-key=${process.env.IP_DATA_API_KEY}`, (response) => {
+    let data = '';
+  
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    response.on('end', () => {
+      let myIP = JSON.parse(data).ip
+      // console.log(JSON.parse(data));
+      console.log(`CURRENT IP ADDRESS: ${myIP}`);
+  
+      // https.get(`${myIP}:3000/products`, (response) => {
+      //   let data = '';
+      
+      //   response.on('data', (chunk) => {
+      //     data += chunk;
+      //   });
+      
+      //   response.on('end', () => {
+      //     console.log('json-server data: ', data);
+      //   });
+      // });
+    });
+  });
+}
+
+
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -13,6 +47,7 @@ app.get('/', (req, res) => {
 // RUN THE APP TO SERVE index.html
 app.listen(APPPORT, () => {
     console.log(`HOSTNAME: ${os.hostname()} APP Listening on PORT: ${APPPORT}`)
+    ipHostCheck()
 });
 
 
@@ -26,6 +61,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 const jsonServerPort = process.env.JSONSERVERPORT || 3000
-server.listen(jsonServerPort, () => {
-  console.log(`HOSTNAME: ${os.hostname()} JSON SERVER Listening on PORT: ${jsonServerPort}`)
+const localhost = 'localhost'
+server.listen(jsonServerPort, localhost, () => {
+  console.log(`JSON SERVER RUNNING AT http://${localhost}:${jsonServerPort}`)
 });
