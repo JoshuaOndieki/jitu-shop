9fcc4e0bf4f19e7ab9f1191feafdcb311bc5182c
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-26 00:44:59 +0300

    run json server

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+port = 3000
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(port, ()=> {
+    console.log(`JITU SHOP JSON SERVER LISTENING AT PORT: ${port}`);
+})
