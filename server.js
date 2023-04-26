0346ca878d7906b875805b43f3a361eb49d71244
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-26 15:06:09 +0300

    Setup cors access control headers

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const cors = require('cors')
-server.use(cors())
-
-port = 3000
+const cors = require('./cors')
+const port = 3000
 
 server.use(middlewares)
+server.use(cors())
 server.use(router)
 
 
