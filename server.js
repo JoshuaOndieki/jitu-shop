63c40172c6e8f1e9c76e21f0c51022c2d1ffc580
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-26 14:30:13 +0300

    Use cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors')
 
 port = 3000
 
 server.use(middlewares)
 server.use(router)
+server.use(cors())
 
 server.listen(port, ()=> {
     console.log(`JITU SHOP JSON SERVER LISTENING AT PORT: ${port}`);
