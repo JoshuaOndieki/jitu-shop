8d330fae2abf8992557a4c54c3e9018e31bee802
Joshua Ondieki <oj@joshuaondieki.com>
2023-04-26 15:35:38 +0300

    Use bodyParser to pass cors headers

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const cors = require('./cors')
+const cors = require('cors')
 const port = 3000
 
-server.use(middlewares)
-server.use(cors())
+server.use(cors(), jsonServer.bodyParser, middlewares)
 server.use(router)
 
 
